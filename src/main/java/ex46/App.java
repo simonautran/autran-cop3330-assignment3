e0707d185fb595c28c191345026bfe6bff2d559c
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-20 22:24:22 -0400

    Initial Commit

--- src/main/java/ex46/App.java
+++ src/main/java/ex46/App.java
@@ -2,3 +2,28 @@
  *  UCF COP3330 Summer 2021 Assignment 3 Solution
  *  Copyright 2021 Simon Autran
  */
+package ex46;
+import java.io.*;
+import java.nio.*;
+public class ex46 
+{
+    public static void main(String[] args) 
+    {
+        ex46 app = new ex46();
+        String input = app.readFile();
+        WordCount wordCount = new WordCount();
+        wordCount.countWord(input);
+        wordCount.printWordCount();
+    }
+    public String readFile() 
+    {
+        String input = "";
+        Path filename = Paths.get("src/main/java/ex46/exercise46_input.txt");
+        try {
+            input = Files.readString(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
+}
