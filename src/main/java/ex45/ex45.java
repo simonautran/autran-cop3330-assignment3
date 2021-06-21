c12a1ab13d8ab71787206ba35cb60cad46a0377c
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-20 21:57:03 -0400

    Initial Commit

--- src/main/java/ex45/ex45.java
+++ src/main/java/ex45/ex45.java
@@ -7,14 +7,16 @@ import java.util.Scanner;
 public class ex45 
 {
     private static final Scanner in = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Edit FE = new Edit();
         String out_FileName = getUser_OutputFile_Name();
         String output = FE.replaceWord();
         FE.createOutputFile(out_FileName, output);
         in.close();
     }
-    private static String getUser_OutputFile_Name() {
+    private static String getUser_OutputFile_Name() 
+    {
         System.out.println("What would you like to name the output file?");
         return in.nextLine();
     }
