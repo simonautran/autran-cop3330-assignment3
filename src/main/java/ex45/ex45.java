8b1b3dd80ef3022c780c92d3964de74ab14bb081
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-20 21:55:56 -0400

    Update and rename App.java to ex45.java

---
+++ src/main/java/ex45/ex45.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 3 Solution
+ *  Copyright 2021 Simon Autran
+ */
+package ex45;
+import java.util.Scanner;
+public class ex45 
+{
+    private static final Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Edit FE = new Edit();
+        String out_FileName = getUser_OutputFile_Name();
+        String output = FE.replaceWord();
+        FE.createOutputFile(out_FileName, output);
+        in.close();
+    }
+    private static String getUser_OutputFile_Name() {
+        System.out.println("What would you like to name the output file?");
+        return in.nextLine();
+    }
+}
