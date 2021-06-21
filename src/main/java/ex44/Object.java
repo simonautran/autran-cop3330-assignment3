707f74ef10b7ce84f530f4b3db5c3b8f131238be
simonautran <85454074+simonautran@users.noreply.github.com>
2021-06-20 21:36:36 -0400

    Initial Commit

---
+++ src/main/java/ex44/Object.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 3 Solution
+ *  Copyright 2021 Simon Autran
+ */
+package ex44;
+public class Object 
+{
+    public String name;
+    public double price;
+    public int quantity;
+}
